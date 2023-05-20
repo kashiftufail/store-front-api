6c5831fd456ac95f49bb357fdf29be854bb8d269
kashif tufail <kashif.tufail17@gmail.com>
2023-05-21 00:28:41 +0500

    add devise

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
